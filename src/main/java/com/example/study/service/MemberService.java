651d2043f86b0e8006af1970fe80932f7f15e0b5
psm4171 <kkjjhh4171@naver.com>
2024-02-15 14:01:38 +0900

    feat: MemberService

---
+++ src/main/java/com/example/study/service/MemberService.java
@@ -0,0 +1,34 @@
+package com.example.study.service;
+
+import com.example.study.domain.Member;
+import com.example.study.repository.MemberRepository;
+import com.example.study.repository.MemoryMemberRepository;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m-> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다. ");
+                });
+    }
+
+    public List<Member> findAllMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+
+}
