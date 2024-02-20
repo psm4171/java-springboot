5a92551e24951fd32ec34679124b30dea116b504
psm4171 <kkjjhh4171@naver.com>
2024-02-20 21:47:35 +0900

    modify: MemberService - @Transactional

--- src/main/java/com/example/study/service/MemberService.java
+++ src/main/java/com/example/study/service/MemberService.java
@@ -7,7 +7,9 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional // Jpa를 사용하려면 필요한 트랜잭션이 필요하다.
 public class MemberService {
     private final MemberRepository memberRepository;
 
